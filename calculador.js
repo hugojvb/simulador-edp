86c27a5cfaefb6fb432abb53bfb4a03a70320068
Hugo Batista <hugojvb20@gmail.com>
2021-08-07 21:09:14 +0100

    Changed select

--- calculador.js
+++ calculador.js
@@ -1,3 +1,13 @@
-const simples = require("./tarifas/simples");
-const bihorario = require("./tarifas/bihorario");
-const trihorario = require("./tarifas/trihorario");
+// const simples = require("./tarifas/simples");
+// const bihorario = require("./tarifas/bihorario");
+// const trihorario = require("./tarifas/trihorario");
+
+let options = document.querySelectorAll("#options .option");
+let selectedValue = document.querySelector("#selected_value");
+
+options.forEach((element) => {
+	if (element.children[0].checked) {
+		console.log(element);
+	}
+	console.log(element.children[0].checked);
+});
