3b5bb66367bbcf6475f61e431eaf1deddaa52076
Hugo Batista <hugojvb20@gmail.com>
2021-08-07 22:56:54 +0100

    Restructured file structure

--- calculador.js
+++ calculador.js
@@ -2,12 +2,18 @@
 // const bihorario = require("./tarifas/bihorario");
 // const trihorario = require("./tarifas/trihorario");
 
-let options = document.querySelectorAll("#options .option");
-let selectedValue = document.querySelector("#selected_value");
+const options = document.querySelectorAll("#options .option");
+const selectedValue = document.querySelector("#selected_value");
+const submit_button = document.querySelector(".submit_button");
 
 options.forEach((element) => {
-	if (element.children[0].checked) {
-		console.log(element);
-	}
-	console.log(element.children[0].checked);
+	if (element.children[0].checked) selectedValue.innerHTML = element.innerText;
+	element.addEventListener("click", function (e) {
+		console.log(element.children[0].value);
+	});
+});
+
+// SUBMIT FORM
+submit_button.addEventListener("click", function (e) {
+	e.preventDefault();
 });
