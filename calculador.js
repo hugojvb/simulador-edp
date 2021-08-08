8a28c0b17e909c32504a4ba7c18c615bc41aba32
Hugo Batista <hugojvb20@gmail.com>
2021-08-08 20:28:52 +0100

    Added query selectors for every element

--- calculador.js
+++ calculador.js
@@ -2,8 +2,17 @@
 const optionsDiv = document.querySelector(".options");
 const options = document.querySelectorAll(".options .option");
 const selectedValue = document.querySelector(".selected_value");
-const submitButton = document.querySelector(".submit_button");
 const selectViewButton = document.querySelector(".select_view_button");
+const emptyHours = document.querySelector("#empty_hours");
+const fullHours = document.querySelector("#full_hours");
+const edgeHours = document.querySelector("#edge_hours");
+const submitButton = document.querySelector(".submit_button");
+const results = document.querySelector(".results");
+const simplePrice = document.querySelector(".simple_price");
+const bihourlyPrice = document.querySelector(".bihourly_price");
+const trihourlyPrice = document.querySelector(".trihourly_price");
+const finalResult = document.querySelector(".final_result");
+const retryButton = document.querySelector(".retry");
 
 // POWER SELECT DEFAULT VALUE TO 3.45
 selectedValue.innerHTML = options[2].children[1].innerHTML;
@@ -39,4 +48,19 @@ const calculatePowerCost = () => {
 // SUBMIT FORM
 submitButton.addEventListener("click", function (e) {
 	e.preventDefault();
+
+	let powerPrice = calculatePowerCost();
+	let emptyHoursPrice = emptyHours.value;
+	let fullHoursPrice = fullHours.value;
+	let edgeHoursPrice = edgeHours.value;
+
+	results.style.visibility = "visible";
+	results.scrollIntoView({ behavior: "smooth" });
+});
+
+retryButton.addEventListener("click", function (e) {
+	e.preventDefault();
+
+	results.style.visibility = "hidden";
+	window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 });
